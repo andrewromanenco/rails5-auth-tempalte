ee4b0e5b01060fe184e341b30c7ad55593d0da8d
Andrew Romanenco <andrew@romanenco.com>
2017-01-13 03:20:30 -0500

    Add user id to google service

--- app/services/registration/google_service.rb
+++ app/services/registration/google_service.rb
@@ -1,17 +1,28 @@
 module Registration
+  class InfoNotAvailable < StandardError; end
+
   class GoogleService
     def initialize(access_token)
       @access_token = access_token
     end
 
+    def user_id
+      person.id
+    end
+
     def full_name
+      person.display_name
+    end
+
+    private
+
+    def person
       begin
-        person = GooglePlus::Person.get('me', access_token: @access_token)
-        person.display_name
+        @person ||= GooglePlus::Person.get('me', access_token: @access_token)
       rescue RuntimeError => e
         Rails.logger.error e.message
         Rails.logger.error e.backtrace.join("\n")
-        ''
+        raise InfoNotAvailable
       end
     end
   end
--- test/services/registration/google_service_test.rb
+++ test/services/registration/google_service_test.rb
@@ -2,18 +2,36 @@ require 'test_helper'
 
 module Registration
   class GoogleServiceTest < ActiveSupport::TestCase
-    test "#full_name returns full name for valid token" do
+    test "#user_id returns id for a valid token" do
+      GooglePlus::Person.stubs(:get).returns(OpenStruct.new({id: '12345'}))
+
+      testee = GoogleService.new 'some-token'
+      assert_equal '12345', testee.user_id
+    end
+
+    test "#user_id raises InfoNotAvailable exception on error" do
+      GooglePlus::Person.stubs(:get).raises(RuntimeError)
+
+      testee = GoogleService.new 'some-token'
+      assert_raises InfoNotAvailable do
+        testee.user_id
+      end
+    end
+
+    test "#full_name returns full name for a valid token" do
       GooglePlus::Person.stubs(:get).returns(OpenStruct.new({display_name: 'Full Name'}))
 
       testee = GoogleService.new 'some-token'
       assert_equal 'Full Name', testee.full_name
     end
 
-    test "#full_name returns empty string on error" do
+    test "#full_name raises InfoNotAvailable exception on error" do
       GooglePlus::Person.stubs(:get).raises(RuntimeError)
 
       testee = GoogleService.new 'some-token'
-      assert_equal '', testee.full_name
+      assert_raises InfoNotAvailable do
+        testee.full_name
+      end
     end
   end
 end
