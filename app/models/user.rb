dca312d255b99d17092471a4346292ae7f695c5e
Andrew Romanenco <andrew@romanenco.com>
2017-01-13 18:18:30 -0500

    Refactor user tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
   has_many :o_auths
 
   def self.find_or_create_by_oauth(oauth_service)
-    user = User.joins(:o_auths).where(o_auths: {remote_id: oauth_service.id}).first
+    user = User.joins(:o_auths).where(o_auths: {remote_id: oauth_service.user_id}).first
     return user if user
     user = User.create(full_name: oauth_service.full_name)
-    user.o_auths.create(remote_id: oauth_service.id)
+    user.o_auths.create(remote_id: oauth_service.user_id)
     user.save
     user
   end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,22 +2,26 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   test "#find_or_create_by_oauth returns existing user" do
+    Registration::GoogleService.any_instance.stubs(:user_id).returns('google_remote_id')
+    Registration::GoogleService.any_instance.stubs(:full_name).returns('Some User')
+
     user = nil
     assert_no_difference 'User.count' do
-      user = User.find_or_create_by_oauth(
-        OpenStruct.new({id: 'google_remote_id', full_name: 'name'}))
+      user = User.find_or_create_by_oauth(Registration::GoogleService.new 'access-token')
     end
     assert_equal users(:google_user).id, user.id
   end
 
   test "#find_or_create_by_oauth creates user if none exists" do
+    Registration::GoogleService.any_instance.stubs(:user_id).returns('NON-EXISTING')
+    Registration::GoogleService.any_instance.stubs(:full_name).returns('Some User')
+
     user = nil
     assert_difference ['User.count', 'OAuth.count'] do
-      user = User.find_or_create_by_oauth(
-        OpenStruct.new({id: 'NON-EXISTING', full_name: 'user name'}))
+      user = User.find_or_create_by_oauth(Registration::GoogleService.new 'access-token')
     end
     assert user
-    assert_equal 'user name', user.full_name
+    assert_equal 'Some User', user.full_name
     assert_equal 'NON-EXISTING', user.o_auths.first.remote_id
   end
 end
